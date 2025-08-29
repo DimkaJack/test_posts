94119fc866258a9a64d0fcc0960a386ee99942c3
D. Vorozhtsov <vorozhtsov@internet.ru>
2025-08-29 17:48:30 +0400

    add get, store, update, delete routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Http\Resources\PostResource;
-use App\Models\Post;
 use App\Services\PostService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 final class PostController extends Controller
 {
@@ -31,30 +32,48 @@ final class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        //
+        return new PostResource(
+            resource: $this->postService->store($request->validated())
+        );
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(int $id): PostResource
     {
-        //
+        return new PostResource(
+            resource: $this->postService->get(id: $id)
+        );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(UpdatePostRequest $request, int $id): PostResource
     {
-        //
+        return new PostResource(
+            resource: $this->postService->update(
+                id: $id,
+                data: $request->validated()
+            )
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(int $id): JsonResponse
     {
-        //
+        if ($this->postService->delete($id)) {
+            return response()->json(
+                status: ResponseAlias::HTTP_NO_CONTENT,
+            );
+        }
+
+        return response()->json(
+            data: ['message' => 'Не удалось удалить пост'],
+            status: ResponseAlias::HTTP_INTERNAL_SERVER_ERROR
+        );
     }
 }
--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -13,4 +13,29 @@ final readonly class PostService
     {
         return Post::query()->paginate(10); //todo set params to pagination
     }
+
+    public function get(int $id): Post
+    {
+        return Post::query()->findOrFail($id);
+    }
+
+    public function store(array $data): Post
+    {
+        return Post::query()->create($data);
+    }
+
+    public function update(int $id, array $data): Post
+    {
+        $post = $this->get($id);
+        $post->update($data);
+
+        return $post;
+    }
+
+    public function delete($id): bool
+    {
+        $post = $this->get($id);
+
+        return $post->delete();
+    }
 }
